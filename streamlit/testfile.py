78409e7b591362d834a5334e1f7f5788bd88e98b
Cstan1987stat <139908492+Cstan1987stat@users.noreply.github.com>
2025-02-12 17:08:15 -0600

    Update testfile.py

--- streamlit/testfile.py
+++ streamlit/testfile.py
@@ -3,8 +3,8 @@ import joblib
 import pandas as pd
 
 # Load models
-model = joblib.load("model/lg.joblib.diab")
-column_transformer = joblib.load("preprocessing/column_transformer.joblib.diab")
+model = joblib.load("/mount/src/test/streamlit/model/lg.joblib.diab")
+column_transformer = joblib.load("/mount/src/test/streamlit/preprocessing/column_transformer.joblib.diab")
 
 # Streamlit App Title
 st.title("Diabetes Classification Prediction")
