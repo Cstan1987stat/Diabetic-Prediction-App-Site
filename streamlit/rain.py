08e8c84f0c4af8d3208197d3d2e56d781a355a95
Cstan1987stat <139908492+Cstan1987stat@users.noreply.github.com>
2025-02-16 10:25:48 -0600

    Update rain.py

--- streamlit/rain.py
+++ streamlit/rain.py
@@ -1,13 +1,13 @@
 import streamlit.components.v1 as components
 
-def embed_tableau_dashboard(url):
-    tableau_html = f"""
+def embed_tableau_dashboard():
+    tableau_html = """
     <div class='tableauPlaceholder'>
         <object class='tableauViz' style='display:none;'>
             <param name='host_url' value='https://public.tableau.com/' />
             <param name='embed_code_version' value='3' />
             <param name='site_root' value='' />
-            <param name='name' value='{url}' />
+            <param name='name' value='LogisticRegressionDiabeticPredictionModelDashboard/ModelPerformanceMetricsDashboard' />
             <param name='tabs' value='no' />
             <param name='toolbar' value='yes' />
             <param name='animate_transition' value='yes' />
@@ -20,9 +20,11 @@ def embed_tableau_dashboard(url):
     </div>
     """
     
-    # Use components.html instead of unsafe_allow_html
-    components.html(tableau_html, height=800)
+    components.html(
+        tableau_html,
+        height=800,
+        width=1200
+    )
 
-# Usage
-url = "https://public.tableau.com/views/LogisticRegressionDiabeticPredictionModelDashboard/ModelPerformanceMetricsDashboard?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
-embed_tableau_dashboard(url)
+# Call the function in your Streamlit app
+embed_tableau_dashboard()
