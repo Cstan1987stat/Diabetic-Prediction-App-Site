14f32c5562facafd7f2e3b8708d7e6a4fa3c2cb1
Cstan1987stat <139908492+Cstan1987stat@users.noreply.github.com>
2025-02-16 10:48:53 -0600

    Update rain.py

--- streamlit/rain.py
+++ streamlit/rain.py
@@ -125,7 +125,7 @@ def embed_tableau_dashboard():
     """
     
     # Embed the visualization
-    components.html(tableau_html, height=850, width=1100)
+    components.html(tableau_html, height=895, width=1100)
 
 # Call the function
 embed_tableau_dashboard()
