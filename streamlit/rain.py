8addb5a94aa5fb73cf6defab9fc8da61c32c3378
Cstan1987stat <139908492+Cstan1987stat@users.noreply.github.com>
2025-02-16 10:27:15 -0600

    Update rain.py

--- streamlit/rain.py
+++ streamlit/rain.py
@@ -1,30 +1,24 @@
 import streamlit.components.v1 as components
 
 def embed_tableau_dashboard():
-    tableau_html = """
-    <div class='tableauPlaceholder'>
-        <object class='tableauViz' style='display:none;'>
-            <param name='host_url' value='https://public.tableau.com/' />
-            <param name='embed_code_version' value='3' />
-            <param name='site_root' value='' />
-            <param name='name' value='LogisticRegressionDiabeticPredictionModelDashboard/ModelPerformanceMetricsDashboard' />
-            <param name='tabs' value='no' />
-            <param name='toolbar' value='yes' />
-            <param name='animate_transition' value='yes' />
-            <param name='display_static_image' value='yes' />
-            <param name='display_spinner' value='yes' />
-            <param name='display_overlay' value='yes' />
-            <param name='display_count' value='yes' />
-            <param name='language' value='en-US' />
-        </object>
-    </div>
+    # Get the Tableau visualization URL
+    viz_url = "https://public.tableau.com/views/LogisticRegressionDiabeticPredictionModelDashboard/ModelPerformanceMetricsDashboard"
+    
+    # Construct the embed code using Tableau's JS API
+    tableau_html = f"""
+        <div class='tableauPlaceholder'>
+            <iframe 
+                src='{viz_url}?:embed=yes&:showVizHome=no'
+                width='100%' 
+                height='800' 
+                style='border: none;'>
+            </iframe>
+        </div>
+        <script type='text/javascript' src='https://public.tableau.com/javascripts/api/tableau-2.min.js'></script>
     """
     
-    components.html(
-        tableau_html,
-        height=800,
-        width=1200
-    )
+    # Embed the visualization
+    components.html(tableau_html, height=800, width=None)
 
-# Call the function in your Streamlit app
+# Call the function
 embed_tableau_dashboard()
