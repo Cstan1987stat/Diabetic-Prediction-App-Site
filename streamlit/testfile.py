021499abc1970320919ae9857c8efc1b3bfc188d
Cstan1987stat <139908492+Cstan1987stat@users.noreply.github.com>
2025-05-01 14:16:52 -0500

    Update testfile.py

--- streamlit/testfile.py
+++ streamlit/testfile.py
@@ -107,4 +107,4 @@ if submitted:
     st.info("Note: This tool is for screening purposes only. Consult a medical professional for proper diagnosis.")
 
 st.subheader('Tableau Model Performance Dashboard')
-[Tableau Dashboard]('https://public.tableau.com/app/profile/connor.stanley8849/viz/LogisticRegressionDiabeticModelDashboard/ConfusionMatrixDashboard')
+st.components.v1.iframe("https://public.tableau.com/views/LogisticRegressionDiabeticModelDashboard/ConfusionMatrixDashboard", height=600, width=1000)
